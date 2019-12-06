c5be02213344d7e0cfdd85b7267992deae4b4836
toddmurphy <toddmurphy93@gmail.com>
2019-12-05 20:06:41 -0400

    Display and Dashboard components setup in App

--- baseball-testing/src/App.js
+++ baseball-testing/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Display from './components/Display';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <div>
         <h1>Baseball Scoreboard</h1>
+        <Display />
+        <Dashboard />
       </div>
     </div>
   );
